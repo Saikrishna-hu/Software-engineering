05cd96e3968808ae0478370b3f414d4affafddfb
Saikrishna-hu <117765387+Saikrishna-hu@users.noreply.github.com>
2022-11-08 23:11:20 -0600

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,20 @@
+package tests;
+
+//class
+public class Substraction{
+//getter method
+public Mathematics getSub(){
+return m;
+}
+//setter method
+public void setSub(Mathematics m){
+this.m = m;
+}
+Mathematics m;
+
+public int subNumbers(int a, int b){
+ return m.sub(a,b);
+
+   }
+
+}
