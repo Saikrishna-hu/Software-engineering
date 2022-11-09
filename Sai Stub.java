ff4499b6ba62ea9af2ba1c55526ee98f57647fb7
Saikrishna-hu <117765387+Saikrishna-hu@users.noreply.github.com>
2022-11-09 13:00:23 -0600

    Add files via upload

---
+++ Sai Stub.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class ToSaiStub implements ToSaiStub  {
+
+	public List<String> getservice(String user) {
+	
+	return Arrays.asList(" Apple ", " Orange ", " Banana ", " Fruit ");
+	}
+
+ }
